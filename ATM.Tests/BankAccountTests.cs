73c9d7d2ed0e3654a8b580868b05def22ceb5b6f
Misti <mistidinzy@gmail.com>
2022-12-31 21:19:55 -0600

    ATM tests 3,4 passing

--- ATM.Tests/BankAccountTests.cs
+++ ATM.Tests/BankAccountTests.cs
@@ -30,4 +30,33 @@ public class BankAccountTests
       decimal newBalance = bankaccount.GetBalance();
       Assert.Equal(30, newBalance);
     }
+
+    [Fact]
+    public void Deposit_Adds_Correct_Amount()
+    {
+      // Arrange
+      BankAccount bankaccount = new BankAccount();
+
+      // Act
+      bankaccount.Deposit(30);
+      bankaccount.Deposit(50);
+
+      // Assert
+      decimal newBalance = bankaccount.GetBalance();
+      Assert.Equal(80, newBalance);
+    }
+
+    [Fact]
+    public void Throws_If_Negative_Deposit()
+    {
+      // Arrange
+      BankAccount bankaccount = new BankAccount();
+
+      // Assert
+      Assert.Throws<ArgumentOutOfRangeException>(() =>
+      {
+        // Act
+        bankaccount.Deposit(-1);
+      });
+    }
 }
