4d1a83c1ad3edbbd58957ee53a868789fae6f01a
Misti <mistidinzy@gmail.com>
2023-01-02 16:36:19 -0600

    Second method correctly returns average.

--- Lab3App/Program.cs
+++ Lab3App/Program.cs
@@ -6,11 +6,14 @@ namespace Lab3App
   {
     public static void Main()
     {
-      string input = GetInput();
+      string input = GetProductInput();
       ChallengeOne(input);
+
+      string second = GetSecondInput();
+      ChallengeTwo(second);
     }
 
-    public static string GetInput()
+    public static string GetProductInput()
     {
       string result = "foo";
 
@@ -58,6 +61,71 @@ namespace Lab3App
 
       return product;
     }
+
+    public static string GetSecondInput()
+    {
+      string result = "foo";
+
+      Console.WriteLine("Hello, enter a number between 2 and 10.");
+
+      string? userInput = Console.ReadLine();
+
+      if (userInput != null)
+      {
+        result = userInput;
+      }
+
+      return result;
+
+    }
+
+    public static double ChallengeTwo(string input)
+    {
+      double average = 0;
+      int numCount = int.Parse(input);
+
+      // Input the numbers and store in array
+      double[] numbers = new double[numCount];
+
+      for (int i = 0; i < numCount; i++)
+      {
+        Console.WriteLine("Enter a number: ");
+
+        string? entry = Console.ReadLine();
+
+        if (entry != null)
+        {
+          double num = double.Parse(entry);
+
+          // Check that number is positive
+          while (num < 0)
+          {
+            Console.WriteLine("Number must be positive. Enter a positive number: ");
+
+            num = double.Parse(entry);
+          }
+
+          numbers[i] = num;
+        }
+        else { throw new ArgumentException("User input was null."); }
+      }
+
+      // Calculate average of numbers
+      double sum = 0;
+
+      foreach (double num in numbers)
+      {
+        sum += num;
+      }
+
+      average = sum / numCount;
+
+      Console.WriteLine("The average of the numbers is: " + average);
+
+      return average;
+    }
+
   }
+
 }
 
